eac0b3263dd0f4cecd5819c9ce5ce9b138244684
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-10-30 12:22:22 +0600

    Loaded Services

---
+++ src/components/Package/Package.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, Col, Button } from 'react-bootstrap';
+import { VscStarFull } from "react-icons/vsc";
+
+const Package = (props) => {
+    const { serviceTitle, price, rating, imgUrl, description } = props.package;
+    return (
+        <Col>
+            <Card>
+                <Card.Img variant="top" src={imgUrl} />
+                <Card.Body>
+                    <div className="card-top d-flex justify-content-between">
+                        <div className="left">
+                            <Card.Title>{serviceTitle}</Card.Title>
+                        </div>
+                        <div className="right">
+                            <h4>$ {price}</h4>
+                        </div>
+                    </div>
+                    <div className="rating d-flex align-items-center">
+                        <VscStarFull />
+                        <p className="m-0 ms-1">{rating}</p>
+                    </div>
+                    <Card.Text>
+                        {description.slice(0, 200)}
+                    </Card.Text>
+                    <Button style={{ background: '#ef2853', border: '1px solid #ef2853' }}>Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </Col>
+    );
+};
+
+export default Package;
--- src/components/Packages/Packages.js
+++ src/components/Packages/Packages.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Container, Row, Button } from 'react-bootstrap';
+import Package from '../Package/Package';
 import './Packages.css';
 const Packages = () => {
+    const [packages, setPackages] = useState([]);
+    useEffect(() => {
+        fetch('https://agile-ravine-60330.herokuapp.com/packages')
+            .then(res => res.json())
+            .then(data => setPackages(data));
+    }, [])
     return (
         <div className="packages-section">
             <Container>
                 <h2 className="text-center">Our Tour Packages</h2>
                 <div className="package-cards">
-                    <Row xs={1} md={2} className="g-4">
-                        <Col>
-                            <Card>
-                                <Card.Img variant="top" src="holder.js/100px160" />
-                                <Card.Body>
-                                    <Card.Title>Card title</Card.Title>
-                                    <Card.Text>
-                                        This is a longer card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                    <Row xs={1} md={3} className="g-4">
+                        {
+                            packages.map(pkg => <Package key={pkg._id} package={pkg}></Package>)
+                        }
                     </Row>
                 </div>
             </Container>
